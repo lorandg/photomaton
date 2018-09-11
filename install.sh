2ea513528237f4a423a8f336d04af596311c8306
lorandg <guillaume.lorand@gmail.com>
2018-09-11 22:44:43 +0200

    base takeAPicture on python-gphoto2

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-apt-get install pip
-sudo pip install -r requirements.txt
+apt-get install python-pip
+sudo pip3 install -r requirements.txt
--- service.py
+++ service.py
@@ -5,6 +5,7 @@ from flask import jsonify
 from flask import redirect, url_for, Response
 import json
 from subprocess import call
+import gphoto2 as gphoto
 
 app= Flask(__name__)
 
@@ -25,8 +26,19 @@ def pictures():
 @app.route("/pictures/take", methods=['PUT'])
 def takeApicture():
     
-    call(["gphoto2", "--capture-image-and-download"])
-    
+    #call(["gphoto2", "--capture-image-and-download"])
+    camera = gphoto.check_result(gphoto.gp_camera_new())
+    gphoto.check_result(gphoto.gp_camera_init(camera))
+    print('Capturing image')
+    file_path = gphoto.check_result(gphoto.gp_camera_capture(
+        camera, gphoto.GP_CAPTURE_IMAGE))
+    print('Camera file path: {0}/{1}'.format(file_path.folder, file_path.name))
+    print('Copying image to', target)
+    camera_file = gphoto.check_result(gphoto.gp_camera_file_get(
+            camera, file_path.folder, file_path.name, gphoto.GP_FILE_TYPE_NORMAL))
+    gphoto.check_result(gphoto.gp_file_save(camera_file, "static/img"))
+    gphoto.check_result(gphoto.gp_camera_exit(camera))
+  
     data = {
         "response":"OK"
     }
