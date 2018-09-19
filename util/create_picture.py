7f7e2bf4c013937485e0a5e2635924be1e9c7a68
guillaume lorand <guillaume.lorand@gmail.com>
2018-09-19 21:29:09 +0200

    fix picture created with the same date

--- util/create_picture.py
+++ util/create_picture.py
@@ -1,10 +1,11 @@
 from PIL import Image, ImageDraw, ImageFont
- 
+import time
  
 fnt = ImageFont.truetype('arial.ttf', 60)
  
-for i in reversed(range(1,15)) :
-    img = Image.new('RGB', (100, 100), color = (73, 109, 137))
+for i in range(1,15) :
+    img = Image.new('RGB', (1920, 1080), color = (73, 109, 137))
     d = ImageDraw.Draw(img)
     d.text((30,20), "%d"%i, font=fnt, fill=(255, 255, 0))
-    img.save('../static/img/%d.jpg'%i)    
+    img.save('../static/img/%d.jpg'%i)
+    time.sleep(0.05)
